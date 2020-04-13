467fab951a86e62bee372c75ce6ae5b5fa75bfe9
Paul Harrison <paul@harrison.sh>
2020-04-13 12:11:54 +0100

    Move test client to conftest.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,3 @@
-import pytest
-from fastapi.testclient import TestClient
-
-from bike_charts.main import app
-
-
-@pytest.fixture(scope="module")
-def client():
-    return TestClient(app)
-
-
 def test_root(client):
     response = client.get("/")
     assert response.status_code == 200
